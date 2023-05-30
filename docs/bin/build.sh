c1d9467a99189863238ddf2c8552bf85da720598
Jakub Zalas <jakub@zalas.pl>
2023-05-30 10:35:29 +0200

    Bootstrap the diary

---
+++ docs/bin/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eo pipefail
+
+PROJECT_DIR=$(dirname $(readlink -f $0))/../
+
+function build_html() {
+  name=$1
+  source=$2
+  target=$3
+  printf '%s' "$name "
+  output=$(asciidoctor $source -o $target 2>&1) \
+    && echo "✔︎" \
+    || echo "✗"
+  [[ "$output" -eq "" ]] || echo $output
+}
+
+build_html "Diary (HTML)" "diary/src/index.adoc" "build/diary.html"
