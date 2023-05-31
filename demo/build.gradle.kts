7a322a5d21c7b18e0e63b064a2ca7eeb9adcb350
Jakub Zalas <jakub@zalas.pl>
2023-05-31 09:34:05 +0200

    Learn how to load an image in Compose

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform")
@@ -16,7 +17,7 @@ repositories {
 
 kotlin {
     jvm {
-        jvmToolchain(11)
+        jvmToolchain(19)
         withJava()
     }
     sourceSets {
--- demo/src/jvmMain/kotlin/Main.kt
+++ demo/src/jvmMain/kotlin/Main.kt
@@ -1,28 +1,28 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Image(defaultPhoto(), "Default photo", modifier = Modifier.fillMaxSize())
         }
     }
 }
+
+private fun ColumnScope.defaultPhoto(): ImageBitmap =
+    loadImageBitmap(this::class.java.getResource("/koala.jpg").openStream())
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
