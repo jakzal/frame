3214da855255d051823b9708283c3b0d40d077b6
Jakub Zalas <jakub@zalas.pl>
2023-05-31 16:25:22 +0200

    Use a mutable property to show the current photo

--- demo/src/jvmMain/kotlin/Main.kt
+++ demo/src/jvmMain/kotlin/Main.kt
@@ -1,30 +1,30 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import java.io.InputStream
 
 @Composable
 @Preview
 fun App() {
+    var displayedPhoto by remember { mutableStateOf(defaultPhotoStream()) }
     MaterialTheme {
         Column {
-            Image(defaultPhoto(), "Default photo", modifier = Modifier.fillMaxSize())
+            Image(loadImageBitmap(displayedPhoto), "Default photo", modifier = Modifier.fillMaxSize())
         }
     }
 }
 
-private fun ColumnScope.defaultPhoto(): ImageBitmap =
-    loadImageBitmap(this::class.java.getResource("/koala.jpg").openStream())
+private fun defaultPhotoStream(): InputStream =
+    object {}::class.java.getResource("/koala.jpg").openStream()
 
 fun main() = application {
     val state = rememberWindowState(placement = WindowPlacement.Fullscreen)
