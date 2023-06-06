8f6cb8d1a705cee7b8dac92e63024f1e64380804
Jakub Zalas <jakub@zalas.pl>
2023-06-06 21:23:51 +0100

    Make the test pass

--- app/src/commonMain/kotlin/pl/zalas/frame/app/Main.kt
+++ app/src/commonMain/kotlin/pl/zalas/frame/app/Main.kt
@@ -1,12 +1,21 @@
 package pl.zalas.frame.app
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.loadImageBitmap
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        Image(
+            loadImageBitmap(object {}::class.java.getResource("/koala.jpg").openStream()),
+            "Main Content",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
