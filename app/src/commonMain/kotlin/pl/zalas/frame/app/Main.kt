26740feaa13278de0e2fad31feabdc7e6165980d
Jakub Zalas <jakub@zalas.pl>
2023-06-06 21:42:05 +0100

    Add the main() function to the app project

--- app/src/commonMain/kotlin/pl/zalas/frame/app/Main.kt
+++ app/src/commonMain/kotlin/pl/zalas/frame/app/Main.kt
@@ -7,6 +7,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.runBlocking
 
 @Composable
 @Preview
@@ -17,5 +22,14 @@ fun App() {
             "Main Content",
             modifier = Modifier.fillMaxSize()
         )
+    }
+}
+
+fun main(): Unit = runBlocking {
+    application {
+        val state = rememberWindowState(placement = WindowPlacement.Fullscreen)
+        Window(onCloseRequest = ::exitApplication, title = "Frame", state = state) {
+            App()
+        }
     }
 }
